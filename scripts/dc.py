894decdcc45e5ba6cb0b0dc0c55c4d64765b9013
flp5 <fluentpwn@gmail.com>
2024-07-31 18:53:14 +0100

    fix(scripts): ensure the server isn't filled with bots to prevent crashes

--- scripts/dc.py
+++ scripts/dc.py
@@ -33,45 +33,49 @@ if __name__ == '__main__':
 
     gmdict = list(chain.from_iterable(((get_path(f),"gg"),(get_path(f),"ffa"),(get_path(f),"tdm")) for f in files))
     
-    for f in files:
-        with open(f) as g:
-            i = json.loads(g.read())
-            if i["MapSize"][0] == 96 and i["MapSize"][2] == 160:
-                add_path(f, "sta")
-            if "LinearZones" in i and len(i["LinearZones"]) > 1:
-                add_path(f, "cs")
-                if check_teamspawns(i, "brk"):
-                    add_path(f, "brk")
-            if "FlagSpawns" in i and len(i["FlagSpawns"]) > 1: # read NOTE 3 for the script in README.md (1)
-                if check_teamspawns(i, "ctf"):
-                    add_path(f, "ctf")
-                if check_teamspawns(i, "esc"):
-                    add_path(f, "esc")
-            if "RushStages" in i: # (1)
-                add_path(f, "rush")
-            if "WeaponSpawns" in i: # (1)
-                add_path(f, "hh")
+    for mapfile in files:
+        with open(mapfile) as g:
+            mapjson = json.loads(g.read())
+            if mapjson["MapSize"][0] == 96 and mapjson["MapSize"][2] == 160:
+                add_path(mapfile, "sta")
+            if "LinearZones" in mapjson and len(mapjson["LinearZones"]) > 1:
+                add_path(mapfile, "cs")
+                if check_teamspawns(mapjson, "brk"):
+                    add_path(mapfile, "brk")
+            if "FlagSpawns" in mapjson and len(mapjson["FlagSpawns"]) > 1: # read NOTE 3 for the script in README.md (1)
+                if check_teamspawns(mapjson, "ctf"):
+                    add_path(mapfile, "ctf")
+                if check_teamspawns(mapjson, "esc"):
+                    add_path(mapfile, "esc")
+            if "RushStages" in mapjson: # (1)
+                add_path(mapfile, "rush")
+            if "WeaponSpawns" in mapjson: # (1)
+                add_path(mapfile, "hh")
 
     print("OPEN SE WINDOW")
     time.sleep(10)
     print(gmdict)
     with open(f'{serverloc}\\lobby.json') as l:
-        d = json.loads(l.read())
-        for z, e in enumerate(gmdict):
-            if os.path.isfile(f"{serverloc}\\lobby{z+1}.json"):
+        default_json = json.loads(l.read())
+        default_json["FillWithBots"] = False
+        for i, game_pair in enumerate(gmdict):
+            if os.path.isfile(f"{serverloc}\\lobby{i+1}.json"):
                 continue
-            with open(f"{serverloc}\\lobby{z+1}.json", "w") as w:
-                d["GameModes"]=[e[1]]
-                d["Maps"]=[e[0]]
-                w.write(json.dumps(d))
-            p = psutil.Popen([f"{serverloc}\\sectorsedgeserver.exe", f"{z+1}"])
-            capture_thread = multiprocessing.Process(target = start_capture, args = (f"{e[0]}-{e[1]}.pcapng",))
+            with open(f"{serverloc}\\lobby{i+1}.json", "w") as w:
+                default_json["GameModes"]=[game_pair[1]]
+                default_json["Maps"]=[game_pair[0]]
+                w.write(json.dumps(default_json))
+
+            server_process = psutil.Popen([f"{serverloc}\\sectorsedgeserver.exe", f"{i+1}"])
+
+            capture_thread = multiprocessing.Process(target = start_capture, args = (f"{game_pair[0]}-{game_pair[1]}.pcapng",))
             capture_thread.start()
             time.sleep(5)
+            
             pyautogui.click(700, 20)
             pyautogui.click(760, 220)
             pyautogui.press('backspace', presses = 20)
-            pyautogui.write(f'127.0.0.1:{11811 + z}')
+            pyautogui.write(f'127.0.0.1:{11811 + i}')
             pyautogui.click(760, 370)
             time.sleep(10)
             pyautogui.click(1,1)
@@ -81,5 +85,6 @@ if __name__ == '__main__':
             pyautogui.press('esc')
             pyautogui.click(1000, 660)
             pyautogui.click(1000, 660)
+
             capture_thread.join()
-            p.terminate()
+            server_process.terminate()
